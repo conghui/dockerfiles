e06a1f6abdf2148a2d52f1760e18857e2b52299b
Conghui He <heconghui@gmail.com>
2016-08-26 12:09:19 -0700

    copy startfile for bash from root

--- centos/README.md
+++ centos/README.md
@@ -23,7 +23,16 @@ __EOF__
 You need to pass your current `uid` and `gid` to `LOCAL_USER_ID` and `LOCAL_GROUP_ID` respectively when you run a container.
 
 ```
-docker run -it --rm -e LOCAL_USER_ID=`id -u $USER` -e LOCAL_GROUP_ID=`id -g $USER` -v `pwd`/test:/test myimg bash
+docker run -it --rm \
+  -e USER=`id -un $USER` \
+  -e GROUP=`id -gn $USER` \
+  -e LOCAL_USER_ID=`id -u $USER` \
+  -e LOCAL_GROUP_ID=`id -g $USER` \
+  -e "TERM=xterm-256color" \
+  --net=host \
+  --env=DISPLAY \
+  --volume=${HOME}/.Xauthority:/home/$USER/.Xauthority:rw \
+  myimg bash
 ```
 
 ### Reference
--- centos/entrypoint.sh
+++ centos/entrypoint.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
 # Add local user
-# Either use the LOCAL_USER_ID and LOCAL_GROUP_ID if passed in at runtime or
+# Either use the LOCAL_USER_ID, LOCAL_GROUP_ID, USER and GROUP if passed in at runtime or
 # fallback
 
 USER_ID=${LOCAL_USER_ID:-9001}
 GROUP_ID=${LOCAL_GROUP_ID:-9001}
-USER=user
-GROUP=user
+USER=${USER:-user}
+GROUP=${GROUP:-user}
+ROOTPWD=123456
 
+# set root password
+echo "The root passwd is: $ROOTPWD"
+echo "root:$ROOTPWD" | chpasswd
+
+# create user
 echo "Starting with UID: $USER_ID, GID: $GROUP_ID"
 groupadd -f -g $GROUP_ID $GROUP
-useradd --shell /bin/bash -u $USER_ID -g $GROUP_ID -o -c "Docker User" -m $USER
+useradd --shell /bin/bash -u $USER_ID -g $GROUP_ID -o -c "Docker User" $USER -M
+mkdir -p /home/$USER
+cp /root/.bash* /home/$USER && chown $USER:$GROUP /home/$USER -R
+
 export HOME=/home/$USER
 
 exec /usr/local/bin/gosu $USER "$@"
